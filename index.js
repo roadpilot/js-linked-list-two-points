850c970d3bab09ef990917d1e9569ca829eb1f6e
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-10-15 01:32:51 +0000

    algo club solution

--- index.js
+++ index.js
@@ -6,3 +6,40 @@ The number of nodes in the list is sz.
 0 <= Node.val <= 100
 1 <= n <= sz
 */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+    let left = head
+    let right = head
+    let i = 0
+    
+    if (!head.next) return null
+    
+    while (i++ < n) {
+        right = right.next
+    }
+    
+    if (right === null) {
+        return head = head.next
+    } 
+    
+    while (right && right.next) {
+        right = right.next
+        left = left.next
+    }
+    
+    left.next = left.next.next
+    
+    return head        
+};
